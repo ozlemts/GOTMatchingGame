a70094c739b50e5f9b5aef5480c62fdc72139f69
OzlemTS <ozlemturkerii@gmail.com>
2019-12-30 23:39:24 +0300

    score and start

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GOT CARD MATCHING GAME
 This is the js exercise project for JS Study Group 
-[Demo](http://eatable-crack.surge.sh)
+[Demo](gotCardMatching.surge.sh)
 ![readmeIMG](https://user-images.githubusercontent.com/12174223/71591041-d3851300-2b3b-11ea-9ecd-ba5fd00e92f8.png)
 
 * Images from  David M. Buisán
--- js/app.js
+++ js/app.js
@@ -1,39 +1,45 @@
 
 let cardOrder = [0,0,1,1,2,2,3,3,4,4,5,5];
 let shuffled = [];
-let chosen = [0,0];
+let chosenKeys = [0,0];
 let move = 0;
-let el = [];
+let choosenCards = [];
 let score = 0;
-let allCard = document.querySelector(".card_backs");
+let allCards = document.querySelector(".card_backs");
 
 //shuffle the cards
 shuffled = shuffle(cardOrder);
-document.querySelectorAll(".card_front").forEach(e => {e.src = "img/" + shuffled[e.getAttribute('data_key')] + ".png"}); 
+document.querySelectorAll(".card_front").forEach(e => { e.src = "img/" + shuffled[e.getAttribute('data_key')] + ".png" }); 
 
 //open the clicked card, take the number
 document.querySelector(".card_backs").addEventListener('click', (e)=> {
     if(e.target.nodeName =='IMG'){
-        el[move] = e.target;
-        el[move].classList.toggle('removeCover');
-        chosen[move] = e.target.getAttribute('data_key');
+        choosenCards[move] = e.target;
+        choosenCards[move].classList.toggle('removeCover');
+        chosenKeys[move] = e.target.getAttribute('data_key');
         move = move + 1;
         if (move > 1 ) {
             move = 0;
-            if (shuffled[chosen[0]] == shuffled[chosen[1]]){
+            if (shuffled[chosenKeys[0]] == shuffled[chosenKeys[1]]) {
             console.log("They are same");
             score = score + 1;
+            if (score == 6) {
+                setTimeout (()=>{ location.reload(); }, 3000);
+            }
             document.querySelector(".score").innerText = 'Score: ' + score;
+            
             } else {
-                allCard.classList.toggle('lock');
-                setTimeout (()=>{ el[0].classList.toggle('removeCover');
-                                  el[1].classList.toggle('removeCover');
-                                  allCard.classList.toggle('lock');}, 500);
+                allCards.classList.toggle('preventClick');
+                setTimeout (()=>{ choosenCards[0].classList.toggle('removeCover');
+                                  choosenCards[1].classList.toggle('removeCover');
+                                  allCards.classList.toggle('preventClick');}, 300);
             } 
         }
     }
 
 })
+
+
 
 function shuffle(array) {
     var copy = [], n = array.length, i;
